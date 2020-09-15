f184d5ee612416dff85b5ff1e79fb79f91153fd4
Vikas Adiwal <vikas.adiwal@qio.io>
2020-09-15 23:37:37 +0530

    added auth client and parent pom

--- src/main/kotlin/com/adiwal/api/ApiApplication.kt
+++ src/main/kotlin/com/adiwal/api/ApiApplication.kt
@@ -2,7 +2,9 @@ package com.adiwal.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
 
+@ComponentScan("com.adiwal.*")
 @SpringBootApplication
 class ApiApplication
 
--- src/main/kotlin/com/adiwal/api/controller/ApiController.kt
+++ src/main/kotlin/com/adiwal/api/controller/ApiController.kt
@@ -1,11 +1,18 @@
 package com.adiwal.api.controller
 
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class ApiController {
 
+    data class Greeting(val name: String)
+
     @GetMapping("/hello")
-    fun hello() : String = "hello"
+    fun helloplain() : Greeting = Greeting("vikas")
+
+    @GetMapping("/api/hello")
+    fun hello(@RequestParam name: String) : Greeting = Greeting(name)
 }
