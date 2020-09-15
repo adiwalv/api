0fae776835d4712a916365fde1897b4bd0800d3b
Vikas Adiwal <vikas.adiwal@qio.io>
2020-09-15 23:38:04 +0530

    optimized imports

--- src/main/kotlin/com/adiwal/api/controller/ApiController.kt
+++ src/main/kotlin/com/adiwal/api/controller/ApiController.kt
@@ -1,6 +1,5 @@
 package com.adiwal.api.controller
 
-import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
