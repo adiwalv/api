e71b96b533172e5ee618558473422676528e80d4
Vikas Adiwal <vikas.adiwal@qio.io>
2020-09-26 02:40:40 +0530

    added search by postid

--- src/main/kotlin/com/adiwal/api/repository/CommentRepository.kt
+++ src/main/kotlin/com/adiwal/api/repository/CommentRepository.kt
@@ -1,10 +1,15 @@
 package com.adiwal.api.repository
 
-import com.adiwal.api.domain.BlogPost
 import com.adiwal.api.domain.Comment
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
+import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
+import org.springframework.data.rest.core.annotation.RestResource
 
 @RepositoryRestResource(path = "comment", collectionResourceRel = "comment", itemResourceRel = "comment")
 interface CommentRepository : MongoRepository<Comment, String> {
+
+    fun findByBlogPostId(@Param("id") blogPostId: String?, pageable: Pageable): Page<Comment?>?
 }
