4ccad2f67ef2e8096bde22d0d86664686c017610
Vikas Adiwal <vikas.adiwal@qio.io>
2020-09-26 02:58:21 +0530

    set audit fields from Audit.java

--- src/main/kotlin/com/adiwal/api/domain/Entities.kt
+++ src/main/kotlin/com/adiwal/api/domain/Entities.kt
@@ -1,20 +1,12 @@
 package com.adiwal.api.domain
 
-import org.springframework.data.annotation.*
-import java.util.*
+import com.adiwal.commons.audit.Audit
+import org.springframework.data.annotation.Id
 
 
 data class BlogPost(@Id var id: String?,
-                    var name: String,
-                    @CreatedBy val user: String?,
-                    @LastModifiedBy val lastModifiedUser: String?,
-                    @CreatedDate val createdDate: Date?,
-                    @LastModifiedDate val lastModifiedDate: Date?)
+                    var name: String) : Audit()
 
 data class Comment(@Id var id: String?,
                    var text: String,
-                   var blogPostId: String,
-                   @CreatedBy val user: String?,
-                   @LastModifiedBy val lastModifiedUser: String?,
-                   @CreatedDate val createdDate: Date?,
-                   @LastModifiedDate val lastModifiedDate: Date?)
+                   var blogPostId: String) : Audit()
--- src/main/kotlin/com/adiwal/api/repository/CommentRepository.kt
+++ src/main/kotlin/com/adiwal/api/repository/CommentRepository.kt
@@ -6,7 +6,6 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.data.repository.query.Param
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
-import org.springframework.data.rest.core.annotation.RestResource
 
 @RepositoryRestResource(path = "comment", collectionResourceRel = "comment", itemResourceRel = "comment")
 interface CommentRepository : MongoRepository<Comment, String> {
