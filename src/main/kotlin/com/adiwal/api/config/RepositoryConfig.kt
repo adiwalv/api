63af1f34c19902c356339b847654fe39004c1e56
Vikas Adiwal <vikas.adiwal@qio.io>
2020-10-23 01:06:56 +0530

    expose ids

---
+++ src/main/kotlin/com/adiwal/api/config/RepositoryConfig.kt
@@ -0,0 +1,15 @@
+package com.adiwal.api.config
+
+import com.adiwal.api.domain.BlogPost
+import com.adiwal.api.domain.Comment
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter
+
+@Configuration
+class RepositoryConfig : RepositoryRestConfigurerAdapter() {
+    override fun configureRepositoryRestConfiguration(config: RepositoryRestConfiguration) {
+        config.exposeIdsFor(BlogPost::class.java)
+        config.exposeIdsFor(Comment::class.java)
+    }
+}
