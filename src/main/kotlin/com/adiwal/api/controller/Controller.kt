b3a5d15bbbe4ed42df5305d20aeb51bfe048a8a8
Vikas Adiwal <vikas.adiwal@qio.io>
2020-10-23 10:09:07 +0530

    added reply to comment

--- src/main/kotlin/com/adiwal/api/controller/Controller.kt
+++ src/main/kotlin/com/adiwal/api/controller/Controller.kt
@@ -3,17 +3,16 @@ package com.adiwal.api.controller
 import com.adiwal.api.domain.Comment
 import com.adiwal.api.domain.CommentThread
 import com.adiwal.api.service.CommentThreadService
-import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
+@RestController
 class Controller(val commentThreadService: CommentThreadService) {
 
-    @PostMapping("/reply")
-    fun replyToThread(@RequestParam id: String, @RequestBody comment: Comment): CommentThread {
+    @PostMapping("/comment/{id}/reply")
+    fun replyToComment(@PathVariable("id") id: String, @RequestBody comment: Comment): CommentThread {
         return commentThreadService.replyToCommentThread(id, comment)
     }
-
 }
